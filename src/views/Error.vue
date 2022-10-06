69974bd351d6d1fbb756d5ced7c3c9a80fbbe3f1
HikariLan <shaokeyibb@126.com>
2022-10-06 15:25:41 +0800

    wip Error and Profile

--- src/views/Error.vue
+++ src/views/Error.vue
@@ -1,8 +1,28 @@
 <template>
-
+  发生了一个错误，请返回上一页，然后稍后再试
 </template>
 
 <script setup>
+import {onMounted} from "vue";
+import {useRouter} from "vue-router";
+
+const router = useRouter()
+
+const emit = defineEmits(['modifytitle', 'modifyicon', 'modifyactions'])
+
+function handleClickAppbarIcon() {
+  router.back()
+}
+
+onMounted(() => {
+  emit('modifytitle', "发生了一个错误")
+  emit('modifyicon', {
+    icon: "mdi-arrow-left",
+    tooltip: "返回上一页",
+    handler: handleClickAppbarIcon
+  })
+  emit('modifyactions', undefined)
+})
 </script>
 
 <style scoped>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,8 +1,44 @@
 <template>
-
+  当你看到这个界面的时候说明你已经登录成功了，但是我暂时懒得写这个界面了，所以，凑活看吧（
+  <br>
+  <v-btn @click="handleClickLogout">退出登录</v-btn>
 </template>
 
 <script setup>
+
+import {fetchX} from "../service/frontend.ts";
+import {backendApiUrl} from "../configurations/config.ts";
+import {useRouter} from "vue-router";
+import {onMounted} from "vue";
+
+const router = useRouter()
+
+function handleClickLogout() {
+  fetchX(backendApiUrl + "/auth/logout", {
+    method: "GET"
+  }).then((res) => {
+    if (res.status === 200) {
+      router.push("/?snakebar=退出登录成功")
+    }
+  })
+}
+
+const emit = defineEmits(['modifytitle', 'modifyicon', 'modifyactions'])
+
+function handleClickAppbarIcon() {
+  router.back()
+}
+
+onMounted(() => {
+  emit('modifytitle', "我的档案")
+  emit('modifyicon', {
+    icon: "mdi-arrow-left",
+    tooltip: "返回上一页",
+    handler: handleClickAppbarIcon
+  })
+  emit('modifyactions', undefined)
+})
+
 </script>
 
 <style scoped>
