d504c9b3a5b4cc2e56936b80c2495f3130376d27
HikariLan <shaokeyibb@126.com>
2022-10-06 15:15:17 +0800

    use production url

--- src/configurations/config.ts
+++ src/configurations/config.ts
@@ -1,4 +1,4 @@
-export const backendApiUrl: string = 'http://localhostwebtest.com/api';
-export const frontendUrl:string = 'http://localhostwebtest.com'
-// export const backendApiUrl: string = 'http://hole.xctra.temp.minecraft.kim/api';
-// export const frontendUrl:string = 'http://hole.xctra.temp.minecraft.kim'
+// export const backendApiUrl: string = 'http://localhostwebtest.com/api';
+// export const frontendUrl:string = 'http://localhostwebtest.com'
+export const backendApiUrl: string = '//hole.xctra.temp.minecraft.kim/api';
+export const frontendUrl:string = '//hole.xctra.temp.minecraft.kim'
