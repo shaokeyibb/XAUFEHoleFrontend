d2244ee5b105f9797d481265e1d565c49deff58a
HikariLan <shaokeyibb@126.com>
2022-10-08 02:11:21 +0800

    fix serviceworker

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', (e) => {
     e.waitUntil(
         caches.open('xaufe-hole').then((cache) => cache.addAll([
-            '/pwa-examples/a2hs/index.html',
+            '/index.html',
         ])),
     );
 });
