d8904223857e6f9f8a74ce3e1fd7ff8eb06ff97c
HikariLan <shaokeyibb@126.com>
2022-10-08 10:01:36 +0800

    deprecated url config

--- src/configurations/config.ts
+++ src/configurations/config.ts
@@ -1,4 +1,6 @@
+// deprecated
+
 // export const backendApiUrl: string = 'http://localhostwebtest.com/api';
 // export const frontendUrl:string = 'http://localhostwebtest.com'
-export const backendApiUrl: string = '//hole.xctra.temp.minecraft.kim/api';
-export const frontendUrl:string = '//hole.xctra.temp.minecraft.kim'
+export const backendApiUrl: string = '/api';
+export const frontendUrl:string = ''
