23a01ee7639d7a38e450c02e1d8352df0bcd8c4a
HikariLan <shaokeyibb@126.com>
2022-10-16 13:43:12 +0800

    update link of footbar

--- src/components/FootBar.vue
+++ src/components/FootBar.vue
@@ -3,7 +3,7 @@
     <v-divider></v-divider>
     <v-card-text>
       XAUFEHole 西财树洞
-      ©{{ new Date().getFullYear() }} <a href="#"><strong>XAUFE Computer Technology Research Association</strong></a>,
+      ©{{ new Date().getFullYear() }} <a href="https://www.xctra.cn" target="_blank"><strong>XAUFE Computer Technology Research Association</strong></a>,
       all rights reserved.
       西财大计算机技术研究协会是一个由学生自发成立的技术研究组织，本组织与西安财经大学无从属关系。
       <a href="https://beian.miit.gov.cn/" target="_blank">陕ICP备2020016655号-2</a>
