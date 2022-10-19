2c34e65a94580183aa1cbeb5ad69d8f081a0e65e
HikariLan <shaokeyibb@126.com>
2022-10-19 08:25:45 +0800

    allow all username instead of edu only

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -93,31 +93,81 @@ const appliedAdditionalParams = reactive({})
 const requireRegistrationCode = ref(false)
 
 const authMethods = [
+  // {
+  //   name: "西安财经大学统一身份认证（不可用）",
+  //   href: "https://cas.xaufe.edu.cn/login?service=" + encodeURIComponent(backendApiUrl) + "/auth/casAuth?redirectUrl=" + btoa(frontendUrl + "/#/?snakebar=" + encodeURIComponent("登录成功，欢迎回来！")),
+  //   method: "DIRECT",
+  //   additionalParams: [
+  //     {
+  //       type: String,
+  //       name: "webVpnCookie",
+  //       required: needWebVpnCookie,
+  //       default: undefined,
+  //       label: "西安财经大学 WebVPN Cookie",
+  //       detail: "由于技术限制，我们需要您在登录前向我们提供您 WebVPN 的 Cookie 信息以确保登录系统正常运行：请使用电脑访问 https://webvpn.xaufe.edu.cn，并使用统一身份认证系统登录您的 WebVPN，接下来，点击您浏览器地址栏左侧的锁型按钮，点击 “Cookies”，顺次展开 “Cookies”，“webvpn.xaufe.edu.cn”，找到 “wengine_vpn_ticketwebvpn_xaufe_edu_cn” 键，将 “内容” 一栏旁的值复制到上方的输入框中，再点击 “登录” 即可完成登录。"
+  //     },
+  //     {
+  //       type: Boolean,
+  //       name: "doShareWebVpnCookie",
+  //       required: needWebVpnCookie,
+  //       default: false,
+  //       label: "向我们共享您的 WebVPN Cookie",
+  //       detail: "是否向使用本站服务的其他用户共享您的 WebVPN Cookie 信息，这将使得您和其他人在下次可以跳过该繁琐的步骤，在您的 WebVPN Cookie 到期前，直接登录本站。我们会按照我们的隐私政策，严格保护您的 Cookie 信息。如您选择不共享，则该 Cookie 将会在本次使用过后被直接丢弃，不会储存。"
+  //     }
+  //   ]
+  // },
+  // {
+  //   name: "西安财经大学教育邮箱登录（推荐）",
+  //   href: backendApiUrl + "/auth/emailAuth",
+  //   method: "POST",
+  //   additionalParams: [
+  //     {
+  //       type: String,
+  //       name: "username",
+  //       required: ref(true),
+  //       default: undefined,
+  //       label: "账号（教育邮箱地址）",
+  //       detail: "西安财经大学教育邮箱地址一般以 @xaufe.edu.cn 或 @mail.xaufe.edu.cn 结尾。任何西安财经大学学生都可以申请一个教育邮箱，如果您还没有一个教育邮箱，请访问西安财经大学一站式服务平台 http://my.xaufe.edu.cn，然后点击顶部的“申请邮箱”按钮申请一个。"
+  //     },
+  //     {
+  //       type: String,
+  //       name: "password",
+  //       password: true,
+  //       required: ref(true),
+  //       default: undefined,
+  //       label: "密码",
+  //       detail: "输入您的西财树洞账号密码（不是您的教育邮箱密码！）。如果您还没有登录过西财树洞，则请选中下方的'注册账号模式'来注册一个账号"
+  //     },
+  //     {
+  //       type: String,
+  //       name: "captcha",
+  //       captcha: true,
+  //       captchaUrl: backendApiUrl + "/captcha/generate",
+  //       required: ref(true),
+  //       default: undefined,
+  //       label: "验证码",
+  //       detail: "请正确输入下方的验证码"
+  //     },
+  //     {
+  //       type: String,
+  //       name: "registrationCode",
+  //       required: requireRegistrationCode,
+  //       default: undefined,
+  //       label: "注册验证码",
+  //       detail: "不出意外的话，我们已将一封包含验证码的邮件发送给您，请查看您的收件箱（有时候它会在垃圾箱中），并将其中的验证码输入到此处。"
+  //     },
+  //     {
+  //       type: Boolean,
+  //       name: "registerMode",
+  //       required: ref(true),
+  //       default: false,
+  //       label: "注册账号模式",
+  //       detail: "如果您还没有登录过西财树洞，则请选中此项以使用您输入的账号和密码注册一个账号。"
+  //     }
+  //   ]
+  // },
   {
-    name: "西安财经大学统一身份认证（不可用）",
-    href: "https://cas.xaufe.edu.cn/login?service=" + encodeURIComponent(backendApiUrl) + "/auth/casAuth?redirectUrl=" + btoa(frontendUrl + "/#/?snakebar=" + encodeURIComponent("登录成功，欢迎回来！")),
-    method: "DIRECT",
-    additionalParams: [
-      {
-        type: String,
-        name: "webVpnCookie",
-        required: needWebVpnCookie,
-        default: undefined,
-        label: "西安财经大学 WebVPN Cookie",
-        detail: "由于技术限制，我们需要您在登录前向我们提供您 WebVPN 的 Cookie 信息以确保登录系统正常运行：请使用电脑访问 https://webvpn.xaufe.edu.cn，并使用统一身份认证系统登录您的 WebVPN，接下来，点击您浏览器地址栏左侧的锁型按钮，点击 “Cookies”，顺次展开 “Cookies”，“webvpn.xaufe.edu.cn”，找到 “wengine_vpn_ticketwebvpn_xaufe_edu_cn” 键，将 “内容” 一栏旁的值复制到上方的输入框中，再点击 “登录” 即可完成登录。"
-      },
-      {
-        type: Boolean,
-        name: "doShareWebVpnCookie",
-        required: needWebVpnCookie,
-        default: false,
-        label: "向我们共享您的 WebVPN Cookie",
-        detail: "是否向使用本站服务的其他用户共享您的 WebVPN Cookie 信息，这将使得您和其他人在下次可以跳过该繁琐的步骤，在您的 WebVPN Cookie 到期前，直接登录本站。我们会按照我们的隐私政策，严格保护您的 Cookie 信息。如您选择不共享，则该 Cookie 将会在本次使用过后被直接丢弃，不会储存。"
-      }
-    ]
-  },
-  {
-    name: "西安财经大学教育邮箱登录（推荐）",
+    name: "通过电子邮箱登录",
     href: backendApiUrl + "/auth/emailAuth",
     method: "POST",
     additionalParams: [
@@ -126,8 +176,8 @@ const authMethods = [
         name: "username",
         required: ref(true),
         default: undefined,
-        label: "账号（教育邮箱地址）",
-        detail: "西安财经大学教育邮箱地址一般以 @xaufe.edu.cn 或 @mail.xaufe.edu.cn 结尾。任何西安财经大学学生都可以申请一个教育邮箱，如果您还没有一个教育邮箱，请访问西安财经大学一站式服务平台 http://my.xaufe.edu.cn，然后点击顶部的“申请邮箱”按钮申请一个。"
+        label: "电子邮件地址（账号）",
+        detail: "请输入您的电子邮件地址，例如 example@xctra.cn"
       },
       {
         type: String,
@@ -136,7 +186,7 @@ const authMethods = [
         required: ref(true),
         default: undefined,
         label: "密码",
-        detail: "输入您的西财树洞账号密码（不是您的教育邮箱密码！）。如果您还没有登录过西财树洞，则请选中下方的'注册账号模式'来注册一个账号"
+        detail: "输入您西财树洞账号的密码。如果您还没有登录过西财树洞，则请选中下方的'注册账号模式'来注册一个账号"
       },
       {
         type: String,
@@ -162,7 +212,7 @@ const authMethods = [
         required: ref(true),
         default: false,
         label: "注册账号模式",
-        detail: "如果您还没有登录过西财树洞，则请选中此项以使用您输入的账号和密码注册一个账号。"
+        detail: "如果您还没有登录过西财树洞，则请选中此项以使用您输入的电子邮件地址（账号）和密码注册一个账号。"
       }
     ]
   }
